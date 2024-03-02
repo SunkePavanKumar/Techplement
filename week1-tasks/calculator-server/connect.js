c223eddb412734213b8023db2a04865047c99b42
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-03-03 00:43:21 +0530

    COM : Added the user authentication routes

--- week1-tasks/calculator-server/connect.js
+++ week1-tasks/calculator-server/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO_URI + "/techplament");
     console.log(`Connected to the DB`);
   } catch (error) {
     console.log(`Error while connecting to the database ${error}`);
--- week1-tasks/calculator-server/index.js
+++ week1-tasks/calculator-server/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import "dotenv/config";
 import connect from "./connect.js";
 import cors from "cors";
+import userRouter from "./routes/user.router.js";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use("/api/v1/users", userRouter);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, callback);
