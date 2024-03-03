6decfc2a82113357027e5c6214c5d2b01c160c17
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-03-03 21:09:37 +0530

    COM : audit and user authentication is done

--- week1-tasks/calculator-server/index.js
+++ week1-tasks/calculator-server/index.js
@@ -3,11 +3,13 @@ import "dotenv/config";
 import connect from "./connect.js";
 import cors from "cors";
 import userRouter from "./routes/user.router.js";
+import auditRouter from "./routes/audit.route.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/audit", auditRouter);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, callback);
